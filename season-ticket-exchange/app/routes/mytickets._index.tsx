dfd5d71762bdfd58c6fcd6b539bfedc2c40a31c0
danrealednb <realed@dnb.com>
2023-07-05 13:51:24 -0400

    added protection against non verified sellers on adding tickets

--- season-ticket-exchange/app/routes/mytickets._index.tsx
+++ season-ticket-exchange/app/routes/mytickets._index.tsx
@@ -2,25 +2,51 @@ import { FaPlus, FaTicketAlt } from "react-icons/fa";
 import { Link, useLoaderData } from "@remix-run/react";
 import { getUserTickets } from "~/data/tickets.server";
 import TicketList from "~/components/tickets/TicketList";
-import { requireUserSession } from "~/data/auth.server";
+import { requireUserSession, getUserProfile } from "~/data/auth.server";
 
 export default function MyTicketsPage() {
-  const tickets = useLoaderData();
+  const { userId, user, tickets } = useLoaderData();
   const hasTickets = tickets && tickets.length > 0;
+  const userVerified = user.verified;
+
   return (
     <>
       <main className="">
-        <div className="flex justify-center items-center py-5">
-          <Link to="add" className="">
-            <span className="flex px-1 text-justify items-center space-x-2 text-white border-2 rounded">
-              <FaPlus className="text-white" /> Add Tickets
-              <FaTicketAlt className="text-white" />
-            </span>
-          </Link>
-        </div>
+        {userVerified && (
+          <div className="flex justify-center items-center py-5 disabled:opacity-75">
+            <Link to="add" className="">
+              <span className="flex px-1 text-justify items-center space-x-2 text-white border-2 rounded">
+                <FaPlus className="text-white" /> Add Tickets
+                <FaTicketAlt className="text-white" />
+              </span>
+            </Link>
+          </div>
+        )}
+
+        {!userVerified && (
+          <div>
+            <div className="flex justify-center items-center py-5 disabled:opacity-75">
+              <Link
+                to="add"
+                className=""
+                onClick={(event) => event.preventDefault()}
+              >
+                <span className="flex px-1 text-justify items-center space-x-2 text-gray border-2 rounded">
+                  <FaPlus className="text-gray" /> Add Tickets
+                  <FaTicketAlt className="text-gray" />
+                </span>
+              </Link>
+            </div>
+            <p className="text-center text-white">
+              {" "}
+              **You are not verified to sell tickets**
+            </p>
+          </div>
+        )}
 
         {hasTickets && <TicketList tickets={tickets} />}
-        {!hasTickets && (
+
+        {!hasTickets && userVerified && (
           <section id="no-events" className="flex justify-center">
             <div className="grid justify-center text-center">
               <h1 className="text-white">No tickets found</h1>
@@ -41,6 +67,8 @@ export default function MyTicketsPage() {
 
 export async function loader({ request }) {
   const userId = await requireUserSession(request);
+  const user = await getUserProfile(userId);
   const tickets = await getUserTickets(userId);
-  return tickets;
+  //   return tickets;
+  return { userId, user, tickets };
 }
