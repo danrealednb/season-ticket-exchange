66a2d8141973f4bbd1b4a8a5a3e425057149e808
danrealednb <realed@dnb.com>
2023-07-12 19:20:10 -0400

    fixed landing page

--- season-ticket-exchange/app/routes/_index.tsx
+++ season-ticket-exchange/app/routes/_index.tsx
@@ -5,7 +5,8 @@ import SideNav from "~/components/NavigationMenu";
 import { getTicketsCount } from "~/data/tickets.server";
 
 export default function Index() {
-  const { useriId, userCount, ticketCount } = useLoaderData();
+  const { userId, userCount, ticketCount } = useLoaderData();
+  const profileUrl = `/profile/${userId}`;
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
@@ -14,7 +15,7 @@ export default function Index() {
       <h1 className="text-xl font-bold text-white flex justify-center text-center py-10">
         Welcome to the New York Rangers Season Ticket Exchange
       </h1>
-      <div className="grid justify-center">
+      <div className="grid justify-center items-center">
         <p className="text-white text-center px-5">
           Season Ticket Exchange is for the real fans of the game. It's for the
           season ticket holders looking to put their seats in the hands of fans
@@ -31,7 +32,11 @@ export default function Index() {
           </a>
         </p>
         <p className="text-white text-center py-5 flex justify-center text-white space-x-2 pt-5 px-5">
-          Start selling{" "}
+          Fill out your{" "}
+          <Link to={profileUrl} className="underline px-1">
+            profile,
+          </Link>
+          get verified, and start selling{" "}
           <Link to="/mytickets" className="underline px-2">
             <FaTicketAlt className="text-gray text-3xl" />
           </Link>
@@ -60,12 +65,53 @@ export default function Index() {
           <label>Sold Ticket Count:</label>
           <p className="font-bold text-yellow underline">{spiritsCount}</p>
         </div> */}
+        <div>
+          <h2 className="font-bold text-xl underline py-2">FAQS</h2>
+          <label className="font-bold underline" htmlFor="">
+            Ticket Statuses
+          </label>
+          <p>
+            <strong>AVAILABLE</strong> - Ticket is available for purchase
+          </p>
+          <p>
+            <strong>RESERVED</strong> - Ticket has been reserved by a user who
+            wishes to purchase
+          </p>
+          <p>
+            <strong>PENDING</strong> - User has paid for the ticket and is
+            awaiting receipty from seller
+          </p>
+          <p>
+            <strong>SOLD</strong> - Seller has accepted the buyers payment and
+            the ticket is now sold.
+          </p>
+          <label className="font-bold underline" htmlFor="">
+            Paid Statuses
+          </label>
+          <p>
+            <strong>AVAILABLE</strong> - Ticket is available and there is no
+            paid status at this time
+          </p>
+          <p>
+            <strong>PENDING_PAYMENT</strong> - Ticket has been reserved by a
+            buyer but payment has not been made yet.
+          </p>
+          <p>
+            <strong>PENDING_SELLER_ACCEPTANCE</strong> - Buyer has sent the
+            seller a payment for the tickets
+          </p>
+          <p>
+            <strong>PAYMENT_COMPLETE</strong> - Buyer has acknowledged and
+            reeived the payment from the buyer.
+          </p>
+        </div>
       </div>
     </div>
   );
 }
 export async function loader({ request }) {
   const userId = await requireUserSession(request);
+  //TODO: get user session without redirecting
   const userCount = await getUserCount();
   const ticketCount = await getTicketsCount();
   return { userId, userCount, ticketCount };
